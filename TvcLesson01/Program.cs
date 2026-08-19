fbde22af80f64a48f4c66833092b379b0a011dd1
tvchung <hna.tvchung@gmmail.com>
2026-08-19 10:56:06 +0700

    TvcLesson01

---
+++ TvcLesson01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TvcLesson01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello! Chung Trịnh");
+
+            app.Run();
+        }
+    }
+}
---
+++ TvcLesson01Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TvcLesson01Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
