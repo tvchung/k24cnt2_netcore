fd552742fe239af7e4bcbfdbcbedfd7bfb82042b
tvchung <hna.tvchung@gmmail.com>
2026-08-26 10:56:20 +0700

    TvcLesson02

---
+++ TvcLesson02Theory/Controllers/TvcProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TvcLesson02Theory.Models;
+
+namespace TvcLesson02Theory.Controllers
+{
+    public class TvcProductController : Controller
+    {
+        public IActionResult TvcIndex()
+        {
+            // Dữ liệu lưu trong đối  tượng: ViewBag, ViewData, TempData
+            ViewBag.name = "Trịnh Văn Chung";
+            ViewData["productVD"] = "Laptop Dell Vostro";
+            TempData["UNI"] = "Trường Đại học Nguyễn Trãi - NTU";
+
+            return View();
+        }
+
+        public IActionResult GetProduct()
+        {
+            // Tạo mock data product
+            TvcProduct tvcProduct = new TvcProduct()
+            {
+                ProductID = "2400012323",
+                ProductName = "Trịnh Văn Chung",
+                YearRelease=1979,
+                Price = 1000
+            };
+
+            ViewBag.product = tvcProduct;
+            ViewData["product"] = tvcProduct;
+
+            return View("product");
+        }
+    }
+}
---
+++ TvcLesson02Theory/Models/TvcProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace TvcLesson02Theory.Models
+{
+    public class TvcProduct
+    {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int YearRelease { get; set; }
+        public decimal Price { get; set; }
+    }
+}
