9b14d6c4db4105095d0a6455e7539b0669781044
tvchung <hna.tvchung@gmmail.com>
2026-09-05 10:38:31 +0700

    TvcLesson05Views

---
+++ TvcLesson05Views/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TvcLesson05Views.Models;
+
+namespace TvcLesson05Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        // lập trình trên view với Razor code
+        public IActionResult RazorCode() 
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
